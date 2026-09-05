7aad4ead2ed6eb6156ab4574ad93ccd6f5ff8e73
SHIMP123 <shimp45@gmail.com>
2026-09-05 14:01:14 +0800

    Updated matches in routes file

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,9 +1,37 @@
 import Router from 'express';
+import { createMatchSchema } from '../schemas/matches.js';
+import { db } from '../db/db.js';
 
 const matchRouter = Router();
 
 matchRouter.get('/', (req, res) => {
     res.status(200).json({message: 'Matches list.'})
+})
+
+matchRouter.post('/', async (req, res) => {
+    const parse = createMatchSchema.safeParse(req.body);
+    const { data: { startTime, endTime, sport, homeScore, awayScore } } = parse;
+
+    if (!parse.success) {
+        return res.status(400).json({ error: parse.error});
+    }
+
+    try{
+         
+        const [event] = await db.insert(db.matches).values({
+            ...parse.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime, endTime),
+        }).returning();
+
+        res.status(201).json({ data: event });
+    }catch(e){
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
 })
 
 export default matchRouter;
